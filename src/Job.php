3d8704dea784f71eb3578f88d9c51a92ae9280d7
mosaxiv <mosaxiv@gmail.com>
2020-12-23 11:38:34 +0900

    Add PRIORITY

--- src/Job.php
+++ src/Job.php
@@ -10,7 +10,17 @@ class Job
     const STATUS_SUCCESS = "success";
     const STATUS_ERROR = "error";
     const STATUS_KILLED = "killed";
-    const FINISHED_STATUS = [self::STATUS_SUCCESS, self::STATUS_ERROR, self::STATUS_KILLED];
+    const FINISHED_STATUS = [
+        self::STATUS_SUCCESS,
+        self::STATUS_ERROR,
+        self::STATUS_KILLED
+    ];
+
+    const PRIORITY_VERY_LOW = -2;
+    const PRIORITY_LOW = -1;
+    const PRIORITY_NORMAL = 0;
+    const PRIORITY_HIGH = 1;
+    const PRIORITY_VERY_HIGH = 2;
 
     /**
      * job finished check
